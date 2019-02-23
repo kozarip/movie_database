91288f10a10662ca44212914c7f5d1653208922b
Peter Kozari <peter.kozari@wildom-consulting.hu>
2019-02-23 22:22:20 +0100

    +movie details popup

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ console.log('App is running')
 
 import './styles/styles.scss';
 import SearchMovies from './classes/search-movies.js'
+import MovieDetails from './classes/movie-details.js'
 
 const searcher = new SearchMovies(".resultContainer");
+const movieDetails = new MovieDetails("#dialogContainer");
+
 
 document.getElementById("searchForm").addEventListener("submit", (event)=>{
     event.preventDefault()
-    searcher.getMovies(event.target.elements.movie.value,1)
+    searcher.fetchMovies(event.target.elements.movie.value,1)
 })
+searcher.fetchMovies("game",1)
+movieDetails.fetchDetails("348350");
