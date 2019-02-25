8ea4d9d5fd76e404a5583b02bf833c137bc18e63
Peter Kozari <peter.kozari@wildom-consulting.hu>
2019-02-25 21:11:58 +0100

    refactor functions in movieConnector

--- src/classes/MovieConnector.js
+++ src/classes/MovieConnector.js
@@ -1,35 +1,11 @@
 export default class MovieConnector{
     constructor(){
-        this.api_key = "1c5abaaeaa13c66b570ad3042a0d51f";
+        this.api_key = "1c5abaaeaa13c66b570ad3042a0d51f4";
+        this.link = "https://api.themoviedb.org/3/";
     }
 
-    async fetchMovies(title, page){
-        try{
-            const response = await fetch("https://api.themoviedb.org/3/search/multi?api_key="+this.api_key+"4&language=en-US&query="+encodeURI(title)+"&page="+encodeURI(page));
-            return await response.json()
-        }catch(error){
-            console.log(error)
-            alert("Server error, please try again")
-        }
-    }
-
-    async fetchDetails(movieId){
-        try{
-            const response = await fetch("https://api.themoviedb.org/3/movie/"+movieId+"?api_key="+this.api_key+"4&language=en-US")
-            return await response.json()
-        }catch(error){
-            console.log(error)
-            alert("Server error, please try again")
-        }
-    }
-
-    async fetchGenres(){
-        try{
-            const response = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key="+this.api_key+"4&language=en-US")
-            return await response.json()
-        }catch(error){
-            console.log(error)
-            alert("Server error, please try again")
-        }
+    async fetchFromServer(url){
+        const response = await fetch(url)
+        return await response.json()
     }
 }
