13e66f22339089e21ba59746218b01f616ecde4d
Peter Kozari <peter.kozari@wildom-consulting.hu>
2019-02-25 21:35:29 +0100

    templates in movieList

--- src/classes/MovieList.js
+++ src/classes/MovieList.js
@@ -15,18 +15,21 @@ export default class MoviesList{
         console.log(movies);
         console.log(genres);
         this.genres = genres.genres;
-        let template;
+
+        const imdbImage300_450Link = "https://image.tmdb.org/t/p/w300_and_h450_bestv2"
         const valueOrPlaceholder = (value, placeholder) => value ? value : placeholder;
+        const genresTemplate = (movie) => movie.genre_ids ? movie.genre_ids.map( genre_id => this.getGenreNamebyId(genre_id) ).join(' ') : "";
+        let template;
 
         if(movies.results.length > 0){
         template = `
             <div class="movies">
                 ${movies.results.map(movie => `
                 <div class="movie" data-id="${movie.id}">
-                    <img src=https://image.tmdb.org/t/p/w300_and_h450_bestv2${movie.poster_path} />
+                    <img src=${imdbImage300_450Link+movie.poster_path} />
                     <h3> ${valueOrPlaceholder(movie.title, movie.name)}</h3>
                     <time datetime="${valueOrPlaceholder(movie.first_air_date, "")}">${valueOrPlaceholder(movie.first_air_date, "")}</time>
-                    <p>Genres: ${movie.genre_ids ? movie.genre_ids.map( genre_id => this.getGenreNamebyId(genre_id) ).join(' ') : ""}</p>
+                    <p>Genres: ${genresTemplate(movie)}</p>
                 </div>`
                 ).join('')}
             </div>
